e5352626851095afc49272648d2ea0ebd9d44cd5
ByteHamster <info@bytehamster.com>
2018-04-15 13:04:52 +0200

    Improve click handling

--- app/src/main/java/com/bytehamster/preferencesearch/SearchPreference.java
+++ app/src/main/java/com/bytehamster/preferencesearch/SearchPreference.java
@@ -3,16 +3,14 @@ package com.bytehamster.preferencesearch;
 import android.content.Context;
 import android.content.Intent;
 import android.preference.Preference;
+import android.text.InputType;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
-/**
- * @author Hans-Peter Lehmann
- * @version 1.0
- */
-public class SearchPreference extends Preference {
+public class SearchPreference extends Preference implements View.OnClickListener {
     public SearchPreference(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
     }
@@ -34,20 +32,15 @@ public class SearchPreference extends Preference {
         super.onCreateView(parent);
         LayoutInflater li = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View root = li.inflate(R.layout.search_preference, parent, false);
-        root.findViewById(R.id.search).setFocusableInTouchMode(false);
-        root.findViewById(R.id.search).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                getContext().startActivity(new Intent(getContext(), MainActivity.class));
-            }
-        });
-        root.findViewById(R.id.search).setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                getContext().startActivity(new Intent(getContext(), MainActivity.class));
-                return true;
-            }
-        });
+
+        ((EditText) root.findViewById(R.id.search)).setInputType(InputType.TYPE_NULL);
+        root.findViewById(R.id.search).setOnClickListener(this);
+        root.findViewById(R.id.search_card).setOnClickListener(this);
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        getContext().startActivity(new Intent(getContext(), MainActivity.class));
     }
 }
