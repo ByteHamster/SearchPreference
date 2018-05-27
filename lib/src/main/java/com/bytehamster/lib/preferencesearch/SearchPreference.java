572c2e3ffa988b3d117ef035b3af3567fe87a12a
ByteHamster <info@bytehamster.com>
2018-05-27 15:14:19 +0200

    Removed deprecated methods

--- lib/src/main/java/com/bytehamster/lib/preferencesearch/SearchPreference.java
+++ lib/src/main/java/com/bytehamster/lib/preferencesearch/SearchPreference.java
@@ -1,8 +1,6 @@
 package com.bytehamster.lib.preferencesearch;
 
 import android.content.Context;
-import android.support.annotation.IdRes;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceViewHolder;
 import android.text.InputType;
@@ -54,60 +52,5 @@ public class SearchPreference extends Preference implements View.OnClickListener
      */
     public SearchConfiguration getSearchConfiguration() {
         return searchConfiguration;
-    }
-
-    /**
-     * Sets the current activity that also receives callbacks
-     * @param activity The Activity that receives callbacks. Must implement SearchPreferenceResultListener.
-     * @deprecated Use getSearchConfiguration().setActivity() instead.
-     */
-    public void setActivity(AppCompatActivity activity) {
-        getSearchConfiguration().setActivity(activity);
-    }
-
-    /**
-     * Show a history of recent search terms if nothing was typed yet. Default is true
-     * @param historyEnabled True if history should be enabled
-     * @deprecated Use getSearchConfiguration().setHistoryEnabled() instead.
-     */
-    public void setHistoryEnabled(boolean historyEnabled) {
-        getSearchConfiguration().setHistoryEnabled(historyEnabled);
-    }
-
-    /**
-     * Allow to enable and disable fuzzy searching. Default is true
-     * @param fuzzySearchEnabled True if search should be fuzzy
-     * @deprecated Use getSearchConfiguration().setFuzzySearchEnabled() instead.
-     */
-    public void setFuzzySearchEnabled(boolean fuzzySearchEnabled) {
-        getSearchConfiguration().setFuzzySearchEnabled(fuzzySearchEnabled);
-    }
-
-    /**
-     * Show breadcrumbs in the list of search results, containing of
-     * the prefix given in addResourceFileToIndex, PreferenceCategory and PreferenceScreen.
-     * Default is false
-     * @param breadcrumbsEnabled True if breadcrumbs should be shown
-     * @deprecated Use getSearchConfiguration().setBreadcrumbsEnabled() instead.
-     */
-    public void setBreadcrumbsEnabled(boolean breadcrumbsEnabled) {
-        getSearchConfiguration().setBreadcrumbsEnabled(breadcrumbsEnabled);
-    }
-
-    /**
-     * Sets the container to use when loading the fragment
-     * @param containerResId Resource id of the container
-     * @deprecated Use getSearchConfiguration().setFragmentContainerViewId() instead.
-     */
-    public void setFragmentContainerViewId(@IdRes int containerResId) {
-        getSearchConfiguration().setFragmentContainerViewId(containerResId);
-    }
-
-    /**
-     * Begin adding a file to the index
-     * @deprecated Use getSearchConfiguration().index() instead.
-     */
-    public SearchConfiguration.ResourceAdder index() {
-        return getSearchConfiguration().index();
     }
 }
